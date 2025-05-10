76a5ca9364e105571cd2c0c088deac10832e16f8
Luiz Matheus Leão <lmatheusneto@gmail.com>
2025-05-09 22:28:01 -0300

    segundo commit

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"CardButton"},{"p":"<Unnamed>","l":"GameBoard"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
